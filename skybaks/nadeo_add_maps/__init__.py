c9e8d690704ae43cc4a74eaa93205caaefd84367
skybax <tmflame@gmail.com>
2023-07-27 16:14:57 -0500

    Try to get login information out of dedicated_cfg else from settings

--- skybaks/nadeo_add_maps/__init__.py
+++ skybaks/nadeo_add_maps/__init__.py
@@ -17,10 +17,10 @@ class NadeoAddMaps(AppConfig):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.api = NadeoServicesApi(settings.DEDICATED_USERNAME, settings.DEDICATED_PASSWORD)
+        self.api: NadeoServicesApi = None
 
     async def on_init(self) -> None:
-        await self.api.create_session()
+        await self.initialize_api()
 
     async def on_start(self) -> None:
         await self.instance.command_manager.register(
@@ -28,6 +28,53 @@ class NadeoAddMaps(AppConfig):
                     description="Add maps from Nadeo servers using map UIDs")
                 .add_param(name="map_uids", nargs="*", type=str, required=True)
         )
+
+    async def initialize_api(self) -> None:
+        logger.info("Looking for dedicated server credentials...")
+        username: str = ""
+        password: str = ""
+        try:
+            if await self.instance.storage.driver.is_file(os.path.join("UserData", "Config", "dedicated_cfg.txt")):
+                logger.info("Found dedicated_cfg.txt in standard location, parsing for credentials")
+                async with self.instance.storage.open(os.path.join("UserData", "Config", "dedicated_cfg.txt"), "r", encoding="utf-8") as dedicated_file:
+                    text: str = await dedicated_file.read()
+                    # Nadeo XML parser allows invalid characters to exist so we
+                    # need to do this a hacky way
+                    try:
+                        comment_start_index = text.index("<!--")
+                        while comment_start_index >= 0:
+                            comment_end_index = text.index("-->")
+                            text = text[0:comment_start_index] + text[comment_end_index+3:]
+                            comment_start_index = text.index("<!--")
+                    except:
+                        pass
+                    start_index = text.index("<masterserver_account>")
+                    end_index = text.index("</masterserver_account>")
+                    text = text[start_index+22:end_index]
+                    login_start = text.index("<login>")
+                    login_end = text.index("</login>")
+                    password_start = text.index("<password>")
+                    password_end = text.index("</password>")
+                    username = text[login_start+7:login_end].strip()
+                    password = text[password_start+10:password_end].strip()
+                    pass
+        except Exception as e:
+            logger.info("Error getting credentials from dedicated_cfg.txt: %s" % (str(e),))
+
+        if not username and not password:
+            logger.info("Looking for credentials from PyPlanet settings...")
+            try:
+                username = settings.DEDICATED_USERNAME
+                password = settings.DEDICATED_PASSWORD
+            except Exception as e:
+                logger.info("Error getting credentials from PyPlanet settings: %s" % (str(e)))
+
+        if not username and not password:
+            raise Exception("No dedicated credentials found, unable to continue! Specify your dedicated username and password in your PyPlanet setting with DEDICATED_USERNAME and DEDICATED_PASSWORD")
+
+        logger.info("Got login information for dedicated server account with username '%s'" % (username,))
+        self.api = NadeoServicesApi(username, password)
+        await self.api.create_session()
 
     async def add_nadeoservices_maps(self, player: Player, data, **kwargs) -> None:
         try:
